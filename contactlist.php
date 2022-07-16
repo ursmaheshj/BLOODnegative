97b86c5e75b0847a39bc30a6a608316f55623704
Mahesh Jadhav <omkar28399@gmail.com>
2022-07-16 18:05:16 +0530

    local style removed

--- contactlist.php
+++ contactlist.php
@@ -6,15 +6,7 @@
 <link rel="stylesheet" href="styles.css">
 <script type="text/javascript" src="script.js"></script>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-<style>
-.donors{
-	color:black;
-	margin:50px 0px 0px 600px;
-}
-.vpage{
-	background-color:gray;
-}
-</style>
+
 </head>
 <body class="vpage">
  
