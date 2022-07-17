af25b03df86ae10f488417b517bfc28ab00c34fd
Mahesh Jadhav <omkar28399@gmail.com>
2022-07-17 14:49:57 +0530

    class changed

--- gallery.php
+++ gallery.php
@@ -1,7 +1,5 @@
 <html>
 <head>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
 <title>blood donation|gallery</title>
 <link rel="stylesheet" href="styles.css">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
@@ -20,7 +18,7 @@
    </header>
 <div class="divider"></div>
 
-<div class="container image">
+<div class="image">
   <div class="row  justify-content-evenly">
     <div class="col-4 ">
 	<img class="img-fluid" src="image/a1.jpg">
